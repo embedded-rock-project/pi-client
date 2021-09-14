5aa664e5caf13d511af51c405e2b3e3ce47100d8
GenerelSchwerz <xXSG.KingXx@gmail.com>
2021-09-14 11:23:43 -0400

    second update, updated text handling

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
     loop = asyncio.get_event_loop()
     emb = helper.build_embed("hi", fields=(("hi", "hi", False)))
     emb_json = helper.embeds_to_json(emb)
-    resp = loop.run_until_complete(rm.discord_report(json={"content": None, "embeds": emb_json}))
+    resp = rm.handle_request_text(rm.request("POST", "https://httpbin.org/anything", json={"content": None, "embeds": emb_json}))
     print(emb_json)
     print(resp)
     return
