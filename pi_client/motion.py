18c053c1585f1e863ee2b3e502bb0a10161f5696
GenerelSchwerz <xXSG.KingXx@gmail.com>
2021-09-15 23:05:16 -0400

    update once again

--- pi_client/motion.py
+++ pi_client/motion.py
@@ -9,6 +9,7 @@ import RPi.GPIO as GPIO
 from http_reqs import RequestMaker, defaultMaker
 from asyncio import sleep
 from helper.base_sensor import BaseSensor
+import time
 
 
 #pin = 23
@@ -24,13 +25,13 @@ class MotionSensor(BaseSensor):
 
     def __enter__(self):
         self.enabled = True
-        self._await(self.check_if_enabled())
+        self.check_if_enabled()
 
 
     def callback(self):
         defaultMaker.discord_report(json={"content": "There was movement!"})
 
-    async def check_if_enabled(self):
+    def check_if_enabled(self):
         while True:
             if self.enabled and not self.detecting_movement:
                 print("hi")
@@ -39,4 +40,4 @@ class MotionSensor(BaseSensor):
             elif not self.enabled and self.detecting_movement:
                 GPIO.remove_event_detect(self.pin)
                 self.detect_movement = False
-            await sleep(1)
+            time.sleep(1)
