f91ca0af6acdbc52796746d3c0dd8ba874ff3c62
bizzarebroadcast <gavin.c.hawk@gmail.com>
2021-09-20 11:21:07 -0400

    data

---
+++ server/data_Collection.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+# Json format:
+{
+    "sensor": # name of sensor here: string
+    "type": # status_update, warning, error: string
+    "message": # status message here: string
+    "timestamp": datetime.now()
+}
