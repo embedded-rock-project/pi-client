6487de4cb93a2db77fda300c964e09197e67d58a
ImposterDaniel <45050086+ImposterDaniel@users.noreply.github.com>
2021-09-28 04:56:55 -0400

    Update read_config.py

--- config/read_config.py
+++ config/read_config.py
@@ -2,6 +2,7 @@ import json
 
 
 # with open('/'.join(__file__.split('/')[:-2]) + "/config.json") as f:
+#loads json and configs server (virtual server) and discord url
 with open('/'.join(__file__.split('/')[:-1]) + "/config.json") as f:
     config = json.load(f)
     server_base_url = config['server_url']
